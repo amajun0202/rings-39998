1c672bc1f7af33dde973d3a4388765cdb07cdd2c
amajun0202 <chihirokento8888@gmail.com>
2023-11-29 15:35:26 +0900

    登録設定

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -5,6 +5,18 @@ class LocationsController < ApplicationController
     
   end
 
+  def new
+    @location=Location.new
+  end
+
+  def create
+    @location=Location.new(location_params)
+    if @location.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity  
+    end
+  end
 
   private
 
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,5 @@
 class Location < ApplicationRecord
+  require 'active_hash'
   belongs_to :user
   has_many_attached :images
 
@@ -10,15 +11,14 @@ class Location < ApplicationRecord
   validates :requires_id,    presence: true, numericality: { other_than: 1, message: "を選んでください！" }
   validates :images,         presence: true
   validates :title,          presence: true
-  validates :estimated_time, presence: true
-  validates :cost,                           numericality: { only_integer: true, message: "半角数字のみです!"}
+  validates :estimated_time, presence: true                          
   validates :description,    presence: true
   validates :address,        presence: true, uniqueness: true
   validates :phone_number,                   numericality: { only_integer: true, message: "半角数字のみです!"}, length: { in: 10..11, message: "正しい電話番号をお願いします!"}
-  validates :nearest_station
+  validates :nearest_station,                format: { without: /駅/, message: "には「駅」を含めないでください" }
   validates :travel_time,                    numericality: { only_integer: true, message: "半角数字のみです!"}
   validates :business_hours, presence: true
-  validates :official_url,                   format: { with: URI::DEFAULT_PARSER.make_regexp, message: "は正しいURLの形式で入力してください!" }
+  validates :official_url,                   allow_blank: true, format: { with: URI::DEFAULT_PARSER.make_regexp, message: "は正しいURLの形式で入力してください!" }
 
   
 end
