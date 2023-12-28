2c2d4b00d5d06a64d8398bfd7c2e0389c90cbf2a
amajun0202 <chihirokento8888@gmail.com>
2023-12-28 10:42:39 +0900

    Userエラーメッセージ設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
          PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-         validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
+         validates_format_of :password, with: PASSWORD_REGEX, message: 'は半角英字と数字の両方を含む必要があります' 
 
          validates :nickname,           presence: true
          validates :introduce,          presence: true
